35b8644f2fdcc2803a4ac41d9e3251f4c7e4aa47
dragfire <asem.devajit@gmail.com>
2019-04-17 10:34:20 -0400

    Move Home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hayum/screens/home/home_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,69 +10,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData.dark(),
-      home: MyHomePage(title: 'Hayum'),
+      home: HomeScreen(title: 'Hayum'),
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _selectedIndex = 0;
-  final _widgetOptions = [
-    Text('Index 0: Home'),
-    Text('Index 2: Feed'),
-    Text('Index 2: Search'),
-    Text('Index 3: Profile'),
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Hayum'),
-      ),
-      body: Center(
-        child: _widgetOptions.elementAt(_selectedIndex),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        items: <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-              icon: Icon(Icons.home), title: Container(height: 0.0)),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.history), title: Container(height: 0.0)),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.search), title: Container(height: 0.0)),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.account_box), title: Container(height: 0.0)),
-        ],
-        currentIndex: _selectedIndex,
-        fixedColor: Colors.green,
-        onTap: _onItemTapped,
-      ),
-    );
-  }
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
   }
 }
