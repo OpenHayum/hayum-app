f081fa3cecdb2b9370afa256a6798b27cc4988c1
dragfire <asem.devajit@gmail.com>
2019-04-28 01:41:33 -0400

    Add todo

--- lib/service/auth_service.dart
+++ lib/service/auth_service.dart
@@ -5,11 +5,13 @@ import 'package:http/http.dart' show Client;
 
 class AuthService {
   Client _client = Client();
-  String _url = "10.0.0.199:8080";
+
+  // TODO: extract this to a config file
+  String _url = "10.0.0.199:8080/api/v1";
 
   Future<Auth> loginUser(Map<String, dynamic> data) async {
     try {
-      final response = await _client.post(new Uri.http(_url, "/api/v1/login"),
+      final response = await _client.post(new Uri.http(_url, "/login"),
           body: jsonEncode(data));
       final statusCode = response.statusCode;
 
